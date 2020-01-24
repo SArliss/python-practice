1a34e365e56207064bef1cd3b5eea2963b483a81
sarliss <sinaramartins@gmail.com>
2020-01-24 10:39:22 -0500

    Testing variable types and for loop

---
+++ loops.py
@@ -0,0 +1,20 @@
+items = [100, 200, 300, 400, 500, 600, 700, 800, 900, 100]
+
+item_len = len(items)
+
+for i in items:
+    print(i)
+
+for i in range(4):
+    print(items[i])
+
+# for range (start, stop, step)
+for i in range(0, len(items), 2):
+    print(items[i])
+
+
+
+
+
+
+
--- practice1.py
+++ practice1.py
@@ -1,2 +1,38 @@
+# this is a comment
+
 print("Hello world!")
+
+a = 2
+b = 3
+sum = a + b
+print(sum)
+# print for console.log
+
+number_of_students = 10
+age = 15
+price = 11.5
+first_name = "Sinara"
+last_name = "Arliss"
+print(number_of_students, age, price, first_name, last_name)
+
+# type command to know the variable type
+print(type(number_of_students))
+
+is_pass = True
+is_fail = False
+print(type(is_pass), type(is_fail))
+
+full_name = first_name + last_name
+print(full_name)
+
+items = []
+items = list
+items = ["item1", "item2", "item3", 1.1, 10]
+print(items[3])
+
+items2 = [1,2,3, [1,2,2, ["Hello"]]]
+print(items2)
+
+items.append("this is a new item")
+print(items)
 
